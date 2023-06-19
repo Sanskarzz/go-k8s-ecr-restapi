8acbef2d82d5dd7cdb39fa355bc45224a2d99250
Sanskarzz <Sanskar.gur@gmail.com>
2023-06-19 14:37:40 +0530

    core project files added

---
+++ handlers/fact.go
@@ -0,0 +1,31 @@
+package handlers
+
+import (
+	"github.com/Sanskarzz/go-k8s-ecs-restapi/database"
+	"github.com/Sanskarzz/go-k8s-ecs-restapi/models"
+	"github.com/gofiber/fiber/v2"
+)
+
+func Home(c *fiber.Ctx) error {
+	return c.SendString("Hello, World!")
+}
+
+func CreateFact(c *fiber.Ctx) error {
+	fact := new(models.Fact)
+	if err := c.BodyParser(fact); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	database.DB.Db.Create(&fact)
+
+	return c.Status(200).JSON(fact)
+}
+
+func GetFacts(c *fiber.Ctx) error {
+	facts := []models.Fact{}
+	database.DB.Db.Find(&facts)
+
+	return c.Status(200).JSON(facts)
+}
